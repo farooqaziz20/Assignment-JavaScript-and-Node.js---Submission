9c2e1944f3ace8ee836a8259d7f371840bf9e3d6
farooqaziz20 <50719873+farooqaziz20@users.noreply.github.com>
2022-10-02 17:19:59 +0500

    Initial commit

---
+++ Question2-personal-message.js
@@ -0,0 +1,3 @@
+
+        var name="Farooq Aziz";
+        console.log(`Hello ${name}, would you like to learn some Python today?`);
---
+++ Question7-number-eight.js
@@ -0,0 +1,8 @@
+//code for addition
+console.log(4 + 4);
+//code for multiplication
+console.log(2 * 4);
+//code for subtractions
+console.log(24 - 16);
+//code for division
+console.log(64 / 8);
